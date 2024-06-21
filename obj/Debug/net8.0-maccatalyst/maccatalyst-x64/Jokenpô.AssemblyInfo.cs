f67e37d793a0299eb617874defedadbddfbd0b6e
Ana e Carlos <anatevezvitoriarosseto@gmail.com>
2024-06-21 17:48:17 -0300

    Jokenpo

--- obj/Debug/net8.0-maccatalyst/maccatalyst-x64/Jokenpô.AssemblyInfo.cs
+++ obj/Debug/net8.0-maccatalyst/maccatalyst-x64/Jokenpô.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Jokenpô")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+7aa78d23542b6667a874182c5ff1653e26f57489")]
 [assembly: System.Reflection.AssemblyProductAttribute("Jokenpô")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Jokenpô")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
